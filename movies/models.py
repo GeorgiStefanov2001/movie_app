ca44767718c3ff56127e55997c2aa7cf526ba98a
georgi.stefanov <georgi.stefanov2001@gmail.com>
2019-02-17 18:45:05 +0200

    Created a 'Movie' model and set up the basic admin site

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length = 200)
+    genre = models.CharField(max_length = 200)
+    rating = models.CharField(max_length = 20)
+    release_date = models.DateField(auto_now = False, auto_now_add = False)
+    score = models.DecimalField(max_digits = 3, decimal_places = 1, blank = True, null = True)
+
+    def __str__(self):
+        return self.title
