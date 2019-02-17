fe63664a2bf7089e998d4a4a4ad5656835e5a8eb
georgi.stefanov <georgi.stefanov2001@gmail.com>
2019-02-17 19:01:15 +0200

    Removed the unnecessary 'blank=true' option from the score field so we don't have to initialize it

--- movies/models.py
+++ movies/models.py
@@ -6,7 +6,7 @@ class Movie(models.Model):
     rating = models.CharField(max_length = 20)
     description = models.TextField(null = True)
     release_date = models.DateField(auto_now = False, auto_now_add = False)
-    score = models.DecimalField(max_digits = 3, decimal_places = 1, blank = True, null = True)
+    score = models.DecimalField(max_digits = 3, decimal_places = 1, null = True)
 
     def __str__(self):
         return self.title
