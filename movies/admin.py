17f933a18ad6e70e3ab36582af62e51e037a535c
georgi.stefanov <georgi.stefanov2001@gmail.com>
2019-03-01 22:22:24 +0200

    Added relations and configured the admin site

--- movies/admin.py
+++ movies/admin.py
@@ -1,18 +1,30 @@
 from django.contrib import admin
 from .models import Movie, Celebrity #importing our models
 
+class MoviesInline(admin.TabularInline):
+    model = Movie
+    extra = 1 #for now its 1 because I don't have many movies added
+
+
 class MovieAdmin(admin.ModelAdmin): #making a MovieAdmin class so we can pass it when registering a model
     #fieldsets configures the way we view the options when creating a new model (it splits them into sections like 'General Information' or 'Other', etc.)
     fieldsets = [
         (None, {'fields': ['title']}), #there will be no section because this is our 'main' field (personal preference)
         ('General Information', {'fields': ['genre','rating','release_date']}), #grouping all the general info fields
         ('Other', {'fields': ['description']}), #same thing is applied here
+        ('Celebrities', {'fields': ['director']}),
     ]
     list_display = ('title','genre','rating','release_date') #list_display determines the models' fields we see when we view all the models we have created
     list_filter = ['genre','rating'] #using list_filter we can filter by any criteria we want (here we can filter the movies by 'genre' and 'rating') 
-    search_fields = ['title','genre','release_date'] #using search_fields we can search for models based on some of their fields
+    search_fields = ['title','genre','release_date','director'] #using search_fields we can search for models based on some of their fields
 
+class CelebrityAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Personal Info',{'fields': ['first_name','last_name','age']}),
+        ('Carrer Highlights', {'fields': ['career_highlight']}),
+    ]
+    inlines = [MoviesInline]    
 #this is how we register our models so we can work with them using the admin interface django provides 
 admin.site.register(Movie, MovieAdmin) #passing the MovieAdmin that was created earlier so we have more control and better interaction with the models we created
-admin.site.register(Celebrity)
+admin.site.register(Celebrity, CelebrityAdmin)
 
--- movies/models.py
+++ movies/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+class Celebrity(models.Model):
+
+    first_name = models.CharField(max_length = 140)
+    last_name = models.CharField(max_length = 140)
+    age = models.IntegerField()
+    job = models.CharField(max_length = 140)
+
+    career_highlight = models.TextField(default = "N/A")
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 #this is how we create a model
 class Movie(models.Model):
     #inside this class we create all the fields we want our model to have
@@ -19,19 +31,10 @@ class Movie(models.Model):
     #decimal_places = 1 means that all numbers will be formated like so '7.0' or '5.4', etc.
     #max_digits takes decimal_places into account
     #so having max_digits = 3 and decimal_places = 1 we can have a a maximum number of '10.0'
-    score = models.DecimalField(max_digits = 3, decimal_places = 1, null = True)
+    score = models.DecimalField(max_digits = 3, decimal_places = 1, null = True, blank = True)
+
+    director = models.ForeignKey(Celebrity, on_delete = models.CASCADE, null = True)
     
     def __str__(self):
         return self.title
 
-class Celebrity(models.Model):
-
-    first_name = models.CharField(max_length = 140)
-    last_name = models.CharField(max_length = 140)
-    age = models.IntegerField()
-    job = models.CharField(max_length = 140)
-
-    career_highlight = models.TextField(default = "N/A")
-
-    def __str__(self):
-        return self.first_name + " " + self.last_name
